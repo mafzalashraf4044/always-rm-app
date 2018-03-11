74252a3a41570faf245c60566c8382267f5c5427
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-03-11 11:11:33 +0500

    assets and fonts load issue fixed

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,5 +1,6 @@
 import * as Expo from "expo";
 import * as React from "react";
+import { Image } from 'react-native';
 import { StyleProvider } from "native-base";
 import { Provider } from "react-redux";
 
@@ -13,6 +14,21 @@ export interface State {
 	isLoading: boolean,
 	isReady: boolean,
 }
+
+function cacheImages(images) {
+	return images.map(image => {
+		if (typeof image === 'string') {
+			return Image.prefetch(image);
+		} else {
+			return Expo.Asset.fromModule(image).downloadAsync();
+		}
+	});
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Expo.Font.loadAsync(font));
+}
+
 export default class Setup extends React.Component<Props, State> {
 	constructor() {
 		super();
@@ -22,21 +38,31 @@ export default class Setup extends React.Component<Props, State> {
 			isReady: false,
 		};
 	}
-	componentWillMount() {
-		this.loadFonts();
-	}
-	async loadFonts() {
-		await Expo.Font.loadAsync({
-			Always_Font: require('../assets/Fonts/SF-Pro-Display-Regular.otf')
-		});
 
-		this.setState({ isReady: true });
-	}
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+			require('../assets/Images/app-bg.jpeg'),
+			require('../assets/Images/header-bg.jpeg'),
+    ]);
+
+    const fontAssets = cacheFonts([{
+			Always_Font: require('../assets/Fonts/SF-Pro-Display-Regular.otf')
+		}]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
 
 	render() {
 		if (!this.state.isReady || this.state.isLoading) {
-			return <Expo.AppLoading />;
+      return (
+        <Expo.AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
 		}
+		
 		return (
 			<StyleProvider style={getTheme(variables)}>
 				<Provider store={this.state.store}>

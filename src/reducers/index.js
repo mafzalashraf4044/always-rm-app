86e4b7525a6fd5972085e0de3b9ce35539c7a194
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-03-20 12:18:37 +0500

    all reducers added to rootreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
 
+import loginReducer from "../container/LoginContainer/reducer";
+import forgotPwdReducer from "../container/ForgotPwdContainer/reducer";
 import homeReducer from "../container/HomeContainer/reducer";
+import storesReducer from "../container/StoresContainer/reducer";
+import sidebarReducer from "../container/SidebarContainer/reducer";
+import addEditStoreReducer from "../container/AddEditStoreContainer/reducer";
+import storeVisitReducer from "../container/StoreVisitContainer/reducer";
 
 export default combineReducers({
-	form: formReducer,
+	loginReducer,
+	forgotPwdReducer,
 	homeReducer,
+	storesReducer,
+	sidebarReducer,
+	addEditStoreReducer,
+	storeVisitReducer
 });

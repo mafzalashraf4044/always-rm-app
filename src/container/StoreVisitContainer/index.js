ca7d18ab39b8c8ed0e142311378968bda654189c
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-03-29 16:38:22 +0500

    data grid onchange added

--- src/container/StoreVisitContainer/index.js
+++ src/container/StoreVisitContainer/index.js
@@ -7,6 +7,8 @@ import StoreVisit from "../../components/StoreVisit";
 
 import { visitForm } from "./data";
 
+const FIRST_INDEX = 0;
+
 export interface Props {
 	navigation: any,
 }
@@ -14,8 +16,55 @@ export interface Props {
 export interface State {}
 
 class StoreVisitContainer extends React.Component<Props, State> {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			visitForm,
+		};
+	}
+
+	addOneDataGridItem = (stepIndex, formLayoutIndex) => {
+		const dataGridTemplate = visitForm[stepIndex].components[formLayoutIndex].components[FIRST_INDEX]
+		const dataGridItems = this.state.visitForm[stepIndex].components[formLayoutIndex].components;
+		const _visitForm = this.state.visitForm;
+		_visitForm[stepIndex].components[formLayoutIndex].components = [...dataGridItems, dataGridTemplate];
+	
+		this.setState({
+			visitForm: _visitForm,
+		});
+	}
+
+	onChange = (value, stepIndex, formFieldIndex, handleChangeData) => {
+		const {formLayoutIndex, formLayoutType} = handleChangeData;
+
+		const _visitForm = this.state.visitForm;
+
+		if (formLayoutType === "fieldset" || formLayoutType === "well") {
+			_visitForm[stepIndex].components[formLayoutIndex].components[formFieldIndex].defaultValue = value;
+		} else if (formLayoutType === "panel") {
+			const {isTable, rowIndex, columnIndex} = handleChangeData;
+
+			if (isTable) {
+				_visitForm[stepIndex].components[formLayoutIndex].components[FIRST_INDEX].rows[rowIndex][columnIndex].components[formFieldIndex].defaultValue = value;
+			}
+		}
+
+		this.setState({
+			visitForm: _visitForm,
+		});
+
+	}
+
 	render() {
-		return <StoreVisit navigation={this.props.navigation} visitForm={visitForm} />;
+		return (
+			<StoreVisit
+				onChange={this.onChange}
+				visitForm={this.state.visitForm}
+				navigation={this.props.navigation}
+				addOneDataGridItem={this.addOneDataGridItem}
+			/>
+		);
 	}
 }
 
